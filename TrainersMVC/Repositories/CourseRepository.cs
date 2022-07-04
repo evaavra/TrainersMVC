e441c588569e5678f6efd8c23bc52a70a7804ed5
Eva Avramidou <eva_avra5@yahoo.gr>
2022-07-04 13:14:13 +0300

    Add Repositories

---
+++ TrainersMVC/Repositories/CourseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrainersMVC.Models;
+
+namespace TrainersMVC.Repositories
+{
+    public class CourseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CourseRepository()
+        {
+            _context = new ApplicationDbContext();
+        }
+    }
+}
---
+++ TrainersMVC/Repositories/TrainerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrainersMVC.Models;
+
+namespace TrainersMVC.Repositories
+{
+    public class TrainerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TrainerRepository()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+    }
+}
